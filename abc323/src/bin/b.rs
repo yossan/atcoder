48b6d13289e1e831cac28f6e0dbccdd81e4d99c5
Kosuke Yoshimoto <kosuke.yoshimoto@gmail.com>
2023-10-08 00:14:23 +0900

    Update abc323

--- abc323/src/bin/b.rs
+++ abc323/src/bin/b.rs
@@ -1,4 +1,22 @@
+/*
+1 から N までの番号が付いた N 人のプレイヤーが総当たり戦をしました。この総当たり戦で行われた試合全てについて、二人の一方が勝ち、もう一方が負けました。
+
+総当たり戦の結果は N 個の長さ N の文字列 S1 ,S2,…,SNによって以下の形式で与えられます。
+
+i !=j のとき、Siの j 文字目は o, x のいずれかであり、o のときプレイヤー i がプレイヤー j に勝ったことを、x のときプレイヤー i がプレイヤー j に負けたことを意味する。
+
+i=j のとき、Siの j 文字目は - である。
+
+総当たり戦で勝った試合数が多いほうが順位が上であり、勝った試合数が同じ場合は、プレイヤーの番号が小さいほうが順位が上となります。 N 人のプレイヤーの番号を順位が高い順に答えてください。
+
+制約
+2≤N≤100
+N は整数
+Siは o, x, - からなる長さ N の文字列
+S1,…,SNは問題文中の形式を満たす
+*/
 use proconio::*;
+use std::cmp::Reverse;
 use std::cmp::Ordering::*;
 
 fn main() {
@@ -6,6 +24,7 @@ fn main() {
 		n: usize,
 		s: [marker::Chars; n],
 	}
+	// (oの数, プレーヤー番号)
 	let mut map = vec![(0usize, 0usize); n];
 	for i in 0..n {
 		map[i] = s[i].iter().fold((0, 0), |acc, &c| { 
@@ -13,21 +32,16 @@ fn main() {
 		});
 	}
 	map.sort_by(|a, b| {
-		if a.0.cmp(&b.0) == Equal {
-			if a.1.cmp(&b.1) == Less {
-				Greater
-			} else if a.1.cmp(&b.1) == Greater {
-				Less
-			} else {
-				Equal
+		match Reverse(a.0).cmp(&Reverse(b.0)) {
+			Equal => {
+				a.1.cmp(&b.1)
+			},
+			x => {
+				x
 			}
-		} else if a.0.cmp(&b.0) == Less {
-			Less
-		} else {
-			Greater
 		}
 	});
-	for i in (0..n).rev() {
+	for i in 0..n {
 		print!("{} ", map[i].1 + 1);
 	}
 	println!("");
--- abc323/src/bin/c.rs
+++ abc323/src/bin/c.rs
@@ -1,59 +1,68 @@
+// 参考 sansen 
+// https://atcoder.jp/contests/abc323/submissions/46288823
+
+/*
+N 人のプレイヤーが参加するプログラミングコンテスト World Tour Finals が行われており、競技時間の半分が過ぎました。
+このコンテストでは M 問の問題が出題されており、問題 i の点数 Aiは 500 以上 2500 以下の 100 の倍数です。
+
+各 i=1,…,N について、プレイヤー i がどの問題を既に解いたかを表す文字列 Siが与えられます。 
+Siは o, x からなる長さ M の文字列で、Siの j 文字目が o のときプレイヤー i は問題 j を既に解いており、x のときまだ解いていません。 
+ただし、どのプレイヤーもまだ全ての問題を解いてはいません。
+
+プレイヤー i の総合得点は、解いた問題の点数の合計に、ボーナス点 i 点を加えた点数として計算されます。
+さて、各 i=1,…,N について以下の質問に答えてください。
+
+* プレイヤー i がまだ解いていない問題を少なくとも何問解くことで、プレイヤー i の総合得点が他のプレイヤー全員の現在の総合得点を上回ることができますか？
+
+なお、問題文中の条件と制約から、プレイヤー i が全ての問題を解くことで、他のプレイヤー全員の現在の総合得点を上回ることができることが証明できます。 このことから、答えは常に定義されることに注意してください。
+*/
 use proconio::*;
-use std::cmp::Reverse;
 
 fn main() {
 	input! {
 		n: usize, // プレーヤー数
 		m: usize, // 問題数
-		mut a: [isize; m], // 各問題のスコア
-		ss: [marker::Chars; n], // 各プレーヤーの結果ox
-	}
-	// 各プレーヤーのスコア結果
-	let mut scores = vec![0isize; n];
-	let mut top_score = 0;
-	for i in 0..n {
-		let s = &ss[i]; 
-		for j in 0..m {
-			if s[j] == 'o' {
-				scores[i] += a[j];
-			}
-		}
-		scores[i] += i as isize + 1;
-		if top_score < scores[i] {
-			top_score = scores[i];
-		}
+		a: [usize; m], // 各問題のスコア
+		s: [marker::Chars; n], // 各プレーヤーの結果ox
 	}
 
-	// スコアと旧順序
-	let mut na = Vec::new();
-	for i in 0..m {
-		na.push((a[i], i));
-	}
-	// スコアが高い順に並べる
-	na.sort_by(|a, b| {
-		Reverse(a.0).cmp(&Reverse(b.0))
+	let scores = s
+		.iter()
+		.enumerate()
+		.map(|(i, s)| {
+			i + 1
+				+ s.iter()
+				.zip(a.iter())
+				.filter(|p| *p.0 == 'o')
+				.map(|p| *p.1)
+				.sum::<usize>()
+		})
+	.collect::<Vec<_>>();
+
+	let max = *scores.iter().max().unwrap();
+
+	// 各問題のスコアの順序
+	let mut order = Vec::from_iter(0..m);
+	// sort_byではない
+	order.sort_by_key(|x| {
+		a[*x]
 	});
 
 	for i in 0..n {
 		let mut score = scores[i];
-		let s = &ss[i];
-		
-		if score == top_score {
-			println!("0");
-		} else {
-			let mut j = 0;
-			// 各問題のスコアと旧番号
-			for &(sc, num) in &na {
-				// 解いていない問題
-				if s[num] != 'o' {
-					score += sc;
-					j += 1;
-				} 
-				if score > top_score {
-					println!("{j}");
-					break;
-				}
+		let mut ans = 0;
+		// スコアが大きい順に取り出す
+		for &x in order.iter().rev() {
+			if score >= max {
+				break;
+			}
+			// 解いたことがない問題かどうか
+			if s[i][x] == 'x' {
+				score += a[x];
+				ans += 1;
 			}
 		}
+		println!("{ans}");
 	}
+
 }
